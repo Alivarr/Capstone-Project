f5dbce494958bf14f673cb7800d3662fb7ad0d40
Alivar <ethanhanson9@gmail.com>
2024-03-28 21:36:26 -0700

    forgot to export

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import useAuth from './useAuth';
 import { Link } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
+import e from 'express';
 
 const Cart = () => {
   const { user } = useAuth();
@@ -81,5 +82,6 @@ const Cart = () => {
       <Link to='/products'>Back to Products</Link>
     </div>
   );
-}
+};
 
+export default Cart;
